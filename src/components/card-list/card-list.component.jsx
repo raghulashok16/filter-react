d695d7bb30c4c8bc24707b8126fd7440e5d28b94
raghulashok16 <114047327+raghulashok16@users.noreply.github.com>
2023-02-16 18:20:52 +0530

    chanded

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,15 +1,12 @@
-import { Component } from "react";
+import Card from '../card/card.component';
+import './card-list.styles.css';
 
-class CardList extends Component {
-    render() {
-        const { workers } = this.props;
-        return (
-            <div>
-                {workers.map((worker) => (
-                    <h1 key={worker.id}>{worker.name}</h1>
-                ))}
-            </div>
-        );
-    }
-}
+const CardList = ({ monsters }) => (
+  <div className='card-list'>
+    {monsters.map((monster) => {
+      return <Card key={monster.id} monster={monster} />;
+    })}
+  </div>
+);
+
 export default CardList;
